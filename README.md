773970d4694c287d3be2735d9cdb9cebf4d6e470
kr-basak <kbasak242@gmail.com>
2020-10-15 19:59:02 +0530

    03

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# Hand-Gesture-Recognition-System
-=======
 # Hand Gesture Recognition System using CNN :white_heart:
 ### :boy: Krishna Basak :boy: Kesavan R :boy: Thevaprakash P :boy: Shinjan Verma
----------
+-------------
 This repository contains all the `codes` for our project titled as **Hand Gesture Recognition System using CNN**. The project focuses on -
 1. Making a Convulation Neural Network (CNN) and train the model with custom dataset (images of gestures).
 2. Getting live gesture recgnition through webcam.
@@ -13,14 +11,18 @@ This repository contains all the `codes` for our project titled as **Hand Gestur
 2. NumPy
 3. Scikit-Learn
 4. Glob
-5. PyAutoGUI
+5. PyAutoGUI *(Optional)*
 6. Tensorflow
 7. Keras
 8. Matplotlib
 
 ## Work Explanation
-- [x] [PalmTracker.py](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/PalmTracker.py) : Use this file to make your own image dataset for training and testing the CNN model. Make necessary changes in *number of images* and *path to image* and then run the proram.
+- [x] [PalmTracker.py](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/PalmTracker.py) : Use this file to make your own image dataset for training and testing the CNN model. Make necessary changes in *number of images* and *path to image* and then run the program.
 - [x] [resize.py](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/resize.py) : Set appropriate path where you previously saved the images and run the code to resize/reshape them for training and testing.
 - [x] [num-trainer.ipynb](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/num-trainer.ipynb) : Run the file to define the *Keras* model and train-test the model using the images.
 - [x] [num-recog.py](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/num-recog.py) : This file will predict/recognize hand gesture realtime.
-- [ ] [controll.py](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/controll.py) : This is an optional functionality. If you want to controll your PC through hand gestures, this might help you. To use this functionality - 1. Import this file in [num-recog.py](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/num-recog.py) and add `controll(predicted_class)` to the line number **149**.
+- [ ] [controll.py](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/controll.py) : This is an optional functionality. If you want to controll your PC through hand gestures, this might help you. To use this functionality - 1. Import [this](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/controll.py) file in [num-recog.py](https://github.com/kr-basak/Hand-Gesture-Recognition-System/blob/main/num-recog.py) and add `controll(predicted_class)` function to the line number *83*.
+
+## Screenshots
+![one-gesture](images/one.png) ![two-gesture](data/two.png) !![three-gesture](data/three.png)
+![four-gesture](data/four.png) ![five-gesture](data/five.png) ![blank-gesture](data/blank.png) !
--- num-recog.py
+++ num-recog.py
@@ -6,72 +6,21 @@ from keras.layers import Activation, Dropout
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 from keras.optimizers import Adam
-
 import cv2
 import numpy as np
 import os
-
 import datetime
 from skimage import io
 import os
 import random
 import matplotlib.pyplot as plt
 import glob
-import pyautogui as p
-
-# define the function
-def controll(data):
-	x, y = p.position()
-	# print(x,y)  #for debugging
-	n=p.onScreen(x,y)
-	# print(n)  #for debugging
-	if (n == True):
-		if (data == 0):
-			print('No task')
-		elif (data == 1):
-			p.press('down')
-		elif (data == 2):
-			p.press('up')
-		elif (data == 3):
-			p.press('volumedown')
-		elif (data == 4):
-			p.press('volumeup')
-		elif (data == 5):
-			print('nummguvytuyuy')
-		else:
-			print('No task assigned')
-
-
-'''
-import getdata as gt
-
-md = keras.models.load_model("numbers.h5")
-
-data_list=['one','two','three','four','five','blank']
-i=0
-j=0
-
-for i in range(0, len(data_list)):
-	path, label = gt.read(j)
-	img=cv2.imread(path)
-	img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	img=cv2.resize(img, (100, 120))
-	img=img.reshape(1, 100, 120, 1)
-	pre=md.predict(np.asarray(img))
-	pre=np.argmax(pre)
-	print('----------- For *'+label+'* prediction is  :: ',pre)
-	j+=1
-	i+=1
-'''
 
 bg= None
-
-#print('\n-----------------------------------------------------------\n')
 
 # To segment the region of hand in the image
 def segment(image, threshold=25):
     global bg
-    # print(type(image))
     # find the absolute difference between background and current frame
     diff = cv2.absdiff(bg.astype("uint8"), image)
 
@@ -89,21 +38,15 @@ def segment(image, threshold=25):
         segmented = max(cnts, key=cv2.contourArea)
         return (thresholded, segmented)
 
-#print('\n-----------------------------------------------------------\n')
-
 # load Model Weights
 def _load_weights():
     try:
         model = keras.models.load_model("numbers.h5")
-        # print(model.summary())
-        # print(model.get_weights())
-        # print(model.optimizer)
+        print(model.summary())
         return model
     except Exception as e:
     	print(e)
     	return None
-
-#print('\n-----------------------------------------------------------\n')
 
 def run_avg(image, aWeight):
     global bg
@@ -113,7 +56,6 @@ def run_avg(image, aWeight):
         return
         
     cv2.accumulateWeighted(image, bg, aWeight)
-#print('\n-----------------------------------------------------------\n')
 
 def getPredictedClass(model):
 
@@ -122,15 +64,8 @@ def getPredictedClass(model):
     gray_image = cv2.resize(gray_image, (100, 120))
 
     gray_image = gray_image.reshape(1, 100, 120, 1)
-
-    # cv2.imshow("test",gray_image)
-
     prediction = model.predict(np.asarray(gray_image))
-
-    # prediction = model.predict(np.asarray(gray_image))
-
     prd=''
-
     predicted_class = np.argmax(prediction)
     if predicted_class == 0:
         prd= "ONE"
@@ -146,10 +81,7 @@ def getPredictedClass(model):
         prd= "BLANK"
 
     print(prd)
-    # controll(predicted_class)
     return prd
-
-#print('\n-----------------------------------------------------------\n')
 
 record = False
 
@@ -159,9 +91,6 @@ if __name__ == "__main__":
 
     # get the reference to the webcam
     camera = cv2.VideoCapture(0)
-
-    #print('\n-----------------------------------------------------------\n')
-
     fps = int(camera.get(cv2.CAP_PROP_FPS))
     # region of interest (ROI) coordinates
     top, right, bottom, left = 10, 350, 225, 590
@@ -169,8 +98,6 @@ if __name__ == "__main__":
     num_frames = 0
     # calibration indicator
     calibrated = False
-
-    #print('\n-----------------------------------------------------------\n')
 
     model = _load_weights()
     k = 0
@@ -186,8 +113,6 @@ if __name__ == "__main__":
 
         # clone the frame
         clone = frame.copy()
-
-        #print('\n-----------------------------------------------------------\n')
 
         # get the height and width of the frame
         (height, width) = frame.shape[:2]
@@ -222,20 +147,14 @@ if __name__ == "__main__":
                 # draw the segmented region and display the frame
                 cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
 
-                #print('\n-----------------------------------------------------------\n')
-
-                # count the number of fingers
-                # fingers = count(thresholded, segmented)
                 if (k % (fps / 6) == 0) and record == True:
-                    cv2.imwrite('Temp.png', thresholded)
+                    cv2.imwrite('temp.png', thresholded)
                     predictedClass = getPredictedClass(model)
                     showData = False
                     cv2.putText(clone, str(predictedClass), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
                 # show the thresholded image
                 cv2.imshow("Thesholded", thresholded)
-
-                #print('\n-----------------------------------------------------------\n')
 
         k = k + 1
         # draw the segmented hand
@@ -247,16 +166,16 @@ if __name__ == "__main__":
         # display the frame with segmented hand
         cv2.imshow("Video Feed", clone)
 
-        #print('\n-----------------------------------------------------------\n')
-
         # observe the keypress by the user
         keypress = cv2.waitKey(1) & 0xFF
 
-        # if the user pressed "q", then stop looping
+        # if the user pressed "q", then terminate
         if keypress == ord("q"):
             break
+        # if the user pressed "s", then start/resume predicting
         if keypress == ord("s"):
         	record = True
+        # if the user pressed "p", then stop predicting
         if keypress == ord("p"):
         	record = False
 
